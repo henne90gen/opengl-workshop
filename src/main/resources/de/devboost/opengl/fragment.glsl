c8ba45c951e853fcc92d7a631b4b8617216989a6
Henne <henne90gen@gmail.com>
2018-09-29 20:23:46 +0200

    Resetting master branch

--- src/main/resources/de/devboost/opengl/fragment.glsl
+++
@@ -1,7 +0,0 @@
-varying vec2 v_UV;
-
-uniform sampler2D v_Texture;
-
-void main() {
-	gl_FragColor = texture2D(v_Texture, v_UV);
-}
--- src/main/resources/de/devboost/opengl/vertex.glsl
+++
@@ -1,9 +0,0 @@
-attribute vec3 a_Position;
-attribute vec2 a_UV;
-
-varying vec2 v_UV;
-
-void main() {
-	gl_Position = gl_ModelViewProjectionMatrix * vec4(a_Position, 1);
-	v_UV = a_UV;
-}
